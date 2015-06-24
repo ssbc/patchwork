db3018c698f68142abf190ee2537bbbd1e90e015
Paul Frazee <pfrazee@gmail.com>
2015-06-24 13:14:52 -0500

    add application menu

--- app/index.js
+++ app/index.js
@@ -12,17 +12,28 @@ var tray
 app.on('ready', function ready () {
   // start sbot
 	require('scuttlebot').init(config, function (err, sbot) {
+    // open the web app
+    shell.openExternal('http://localhost:8008')
+
+    // setup menu
+    Menu.setApplicationMenu(Menu.buildFromTemplate([{
+      label: 'Window',
+      submenu: [
+        { label: 'Open Web App', click: onopen },
+        { label: 'Quit', click: onquit }
+      ]
+    }]))
 
     // setup tray icon
     tray = new Tray(__dirname+'/icon.png')
-    var contextMenu = Menu.buildFromTemplate([
-      { label: 'Open App', click: onopen },
+    tray.setContextMenu(Menu.buildFromTemplate([
+      { label: 'Open Web App', click: onopen },
       { label: 'Quit', click: onquit }
-    ])
-    tray.setContextMenu(contextMenu)
+    ]))
     tray.setToolTip('Secure Scuttlebutt: Running on port 8008')
     tray.on('double-clicked', onopen)
 
+    // menu handlers
     function onopen () {
       shell.openExternal('http://localhost:8008')
     }
