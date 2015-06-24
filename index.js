7dbf0b46a5080c4c3469cb014d6ec807ec4d335e
Paul Frazee <pfrazee@gmail.com>
2015-06-24 10:41:17 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require('app')
+var Tray = require('tray')
+var Menu = require('menu')
+var shell = require('shell')
+var config = require('ssb-config') 
+
+// Report crashes to our server.
+//require('crash-reporter').start();
+
+var tray
+
+app.on('ready', function ready () {
+  // start sbot
+	require('scuttlebot').init(config, function (err, sbot) {
+
+    // setup tray icon
+    tray = new Tray('./icon.png')
+    var contextMenu = Menu.buildFromTemplate([
+      { label: 'Open App', click: onopen },
+      { label: 'Quit', click: onquit }
+    ])
+    tray.setContextMenu(contextMenu)
+    tray.setToolTip('Secure Scuttlebutt: Running on port 8008')
+    tray.on('double-clicked', onopen)
+
+    function onopen () {
+      shell.openExternal('http://localhost:8008')
+    }
+    function onquit () {
+      tray = null
+      sbot.close()
+      process.exit()
+    }
+
+  })
+});
