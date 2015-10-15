c80ea7654ab86baadab04f23fdd277b30292c671
Paul Frazee <pfrazee@gmail.com>
2015-10-15 13:25:44 -0700

    summary now shows most recent reply in second line of body

--- ui/com/msg-list/summary.jsx
+++ ui/com/msg-list/summary.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 import { UserLink, NiceDate } from '../index'
 import { Inline as Content } from '../msg-content'
 import { countReplies } from '../../lib/msg-relation'
+import u from '../../lib/util'
 
 export default class Summary extends React.Component {
-
   onClick() {
     this.props.onSelect(this.props.msg)
   }
 
   render() {
     let msg = this.props.msg
+    let lastMsg = !this.props.forceRaw ? u.getLastThreadPost(msg) : false
     var replies = countReplies(msg)
     replies = (replies === 0) ? '' : ''+replies
     return <div className={'msg-list-item summary'+(this.props.selected ? ' selected' : '')+(msg.hasUnread ? ' unread' : '')} onClick={this.onClick.bind(this)}>
@@ -22,7 +23,12 @@ export default class Summary extends React.Component {
         </div>
         <div className="header-right"><NiceDate ts={msg.value.timestamp} /></div>
       </div>
-      <div className="body"><Content msg={msg} forceRaw={this.props.forceRaw} /></div>
+      <div className="body">
+        <div className="body-line"><Content msg={msg} forceRaw={this.props.forceRaw} /></div>
+        { lastMsg && lastMsg !== msg ?
+          <div className="body-line"><Content msg={lastMsg} /></div> :
+          '' }
+      </div>
     </div>
   }
 }
