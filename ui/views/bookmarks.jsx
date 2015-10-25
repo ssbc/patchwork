068a5a79cadef1211fbbb1c30e6b9b201aca9ffa
Paul Frazee <pfrazee@gmail.com>
2015-10-25 16:05:39 -0700

    style and ui tweaks for toolbars and lists

--- ui/views/bookmarks.jsx
+++ ui/views/bookmarks.jsx
@@ -4,6 +4,13 @@ import MsgList from '../com/msg-list'
 import Oneline from '../com/msg-list/oneline'
 import app from '../lib/app'
 
+const FILTERS = [
+  { label: 'All', fn: msg => true },
+  { label: 'Unread', fn: msg => msg.hasUnread },
+  { label: 'Private Messages', fn: msg => !msg.plaintext },
+  { label: 'Mentions', fn: msg => msg.mentionsUser }
+]
+
 export default class Bookmarks extends React.Component {
   cursor (msg) {
     if (msg)
@@ -15,7 +22,7 @@ export default class Bookmarks extends React.Component {
       <MsgList
         threads
         ListItem={Oneline}
-        listItemHeight={35}
+        filters={FILTERS}
         emptyMsg="You have not bookmarked any messages"
         source={app.ssb.patchwork.createBookmarkStream}
         cursor={this.cursor} />
