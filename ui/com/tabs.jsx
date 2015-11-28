6c360bbcbd4962eff535e43c56bfb41a70b146a1
Paul Frazee <pfrazee@gmail.com>
2015-11-28 16:34:42 -0600

    ui: add features to com/tabs

--- ui/com/tabs.jsx
+++ ui/com/tabs.jsx
@@ -7,7 +7,11 @@ class Tab extends React.Component {
   }
   render() {
     const icon = (this.props.useIcon) ? <i className={'fa fa-'+(this.props.selected?'check-circle-o':'circle-thin')} /> : ''
-    return <a className={this.props.selected?'selected':''} onClick={this.onClick.bind(this)}>
+    let cls = []
+    if (this.props.selected) cls.push('selected')
+    if (this.props.fill) cls.push('flex-fill text-center')
+    if (this.props.option.className) cls.push(this.props.option.className)
+    return <a className={cls.join(' ')} onClick={this.onClick.bind(this)}>
       {icon} {this.props.option.label}
     </a>
   }
@@ -15,8 +19,8 @@ class Tab extends React.Component {
 
 export default class Tabs extends React.Component {
   render() {
-    return <div className={'tabs'+(this.props.vertical?' vertical':'')}>
-      {this.props.options.map((opt,i) => <Tab key={i} useIcon={!!this.props.vertical} option={opt} selected={opt === this.props.selected} onClick={this.props.onSelect} />)}
+    return <div className={'tabs'+(this.props.vertical?' vertical':'')+(this.props.fill?' flex':'')}>
+      {this.props.options.map((opt,i) => <Tab key={i} useIcon={!!this.props.vertical} fill={this.props.fill} option={opt} selected={opt === this.props.selected} onClick={this.props.onSelect} />)}
     </div>
   }
 }
