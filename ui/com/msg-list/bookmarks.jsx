6e8ae407e3635d92d2670ecb362eb09e68c9f956
Paul Frazee <pfrazee@gmail.com>
2015-12-09 16:51:16 -0600

    ui: readd filters toolbar to rightnav views

--- ui/com/msg-list/bookmarks.jsx
+++ ui/com/msg-list/bookmarks.jsx
@@ -1,7 +1,8 @@
 'use babel'
 import React from 'react'
 import MsgList from '../msg-list'
-import Summary from '../msg-view/Summary'
+import Summary from '../msg-view/summary'
+import Tabs from '../tabs'
 import app from '../../lib/app'
 
 const FILTERS = [
@@ -11,16 +12,36 @@ const FILTERS = [
 ]
 
 export default class Bookmarks extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { filter: FILTERS[0] }
+  }
+
   cursor (msg) {
     if (msg)
       return [msg.ts, false]
   }
+
+  onSelectFilter(filter) {
+    this.setState({ filter: filter }, () => {
+      this.refs.list.reload()
+    })
+  }
+
   render() {
+    const Toolbar = (props) => {
+      return <div className="toolbar">
+        <Tabs options={FILTERS} selected={this.state.filter} onSelect={this.onSelectFilter.bind(this)} />
+      </div>
+    }
+
     return <div className="bookmarks">
       <MsgList
+        ref="list"
         ListItem={Summary}
+        Toolbar={Toolbar}
+        filter={this.state.filter.fn}
         emptyMsg="No messages."
-        filters={FILTERS}
         source={app.ssb.patchwork.createBookmarkStream}
         cursor={this.cursor} 
         live={{ gt: [Date.now(), false] }} />
--- ui/com/msg-list/notifications.jsx
+++ ui/com/msg-list/notifications.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import MsgList from '../msg-list'
 import Notification from '../msg-view/notification'
+import Tabs from '../tabs'
 import app from '../../lib/app'
 
 const FILTERS = [
@@ -12,16 +13,36 @@ const FILTERS = [
 ]
 
 export default class Notifications extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { filter: FILTERS[0] }
+  }
+
   cursor (msg) {
     if (msg)
       return [msg.ts, false]
   }
+
+  onSelectFilter(filter) {
+    this.setState({ filter: filter }, () => {
+      this.refs.list.reload()
+    })
+  }
+
   render() {
+    const Toolbar = (props) => {
+      return <div className="toolbar">
+        <Tabs options={FILTERS} selected={this.state.filter} onSelect={this.onSelectFilter.bind(this)} />
+      </div>
+    }
+
     return <div className="notifications">
       <MsgList
+        ref="list"
         ListItem={Notification}
-        emptyMsg="No new notifications."
-        filters={FILTERS}
+        Toolbar={Toolbar}
+        filter={this.state.filter.fn}
+        emptyMsg="No notifications."
         source={app.ssb.patchwork.createNotificationsStream}
         cursor={this.cursor} 
         live={{ gt: [Date.now(), false] }} />
