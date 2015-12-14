6787ee0bb7dc1a9c7f8420f6970d1608a1f11059
Paul Frazee <pfrazee@gmail.com>
2015-12-14 15:04:59 -0600

    styles: clean up configuration tools in newsfeed, no longer a second toolbar

--- ui/views/newsfeed.jsx
+++ ui/views/newsfeed.jsx
@@ -7,17 +7,10 @@ import Dipswitch from '../com/form-elements/dipswitch'
 import Tabs from '../com/tabs'
 import MsgList from '../com/msg-list'
 import Card from '../com/msg-view/card'
-import Oneline from '../com/msg-view/oneline'
 import * as HelpCards from '../com/help/cards'
 import app from '../lib/app'
 import social from '../lib/social-graph'
 
-const LISTITEMS = [
-  { label: <i className="fa fa-picture-o" />, Component: Card },
-  { label: <i className="fa fa-th-list" />, Component: Oneline }
-]
-const LISTITEM_CARD = LISTITEMS[0]
-const LISTITEM_ONELINE = LISTITEMS[1]
 
 function followedOnlyFilter (msg) {
   return msg.value.author === app.user.id || social.follows(app.user.id, msg.value.author)
@@ -27,7 +20,6 @@ export default class NewsFeed extends LocalStoragePersistedComponent {
   constructor(props) {
     super(props, 'newsfeedState', {
       isToolbarOpen: true,
-      listItemIndex: 0,
       isFollowedOnly: false
     })
   }
@@ -51,10 +43,6 @@ export default class NewsFeed extends LocalStoragePersistedComponent {
     })
   }
 
-  onSelectListItem(listItem) {
-    this.setState({ listItemIndex: LISTITEMS.indexOf(listItem) })
-  }
-
   onToggleFollowedOnly(b) {
     this.setState({ isFollowedOnly: b }, () => {
       this.refs.list.reload()
@@ -62,21 +50,16 @@ export default class NewsFeed extends LocalStoragePersistedComponent {
   }
 
   render() {
-    const listItem = LISTITEMS[this.state.listItemIndex]
-    const ListItem = listItem.Component
-    const queueNewMsgs = (listItem == LISTITEM_CARD) // only queue new messages for cards
-    const Toolbar = (props) => {
+    const Hero = (props) => {
       if (!this.state.isToolbarOpen) {
         return <div className="toolbar floating">
-          <a className="btn" onClick={this.onToggleToolbar.bind(this)}><i className="fa fa-caret-square-o-down" /></a>
+          <a className="btn" onClick={this.onToggleToolbar.bind(this)}><i className="fa fa-wrench" /></a>
         </div>
       }
       return <div className="toolbar">
-        <a className="btn" onClick={this.onToggleToolbar.bind(this)}><i className="fa fa-caret-square-o-up" /> Collapse</a>
+        <a className="btn" onClick={this.onToggleToolbar.bind(this)}><i className="fa fa-check" /> Done</a>
         <span className="divider" />
-        <Dipswitch label="Followed Only" checked={this.state.isFollowedOnly} onToggle={this.onToggleFollowedOnly.bind(this)} />
-        <span className="divider" />
-        <Tabs options={LISTITEMS} selected={listItem} onSelect={this.onSelectListItem.bind(this)} />
+        <Dipswitch label={this.state.isFollowedOnly?"Followed Only":"All Users"} checked={this.state.isFollowedOnly} onToggle={this.onToggleFollowedOnly.bind(this)} />
       </div>
     }
     const filter = msg => {
@@ -90,11 +73,11 @@ export default class NewsFeed extends LocalStoragePersistedComponent {
         ref="list"
         threads
         composer composerProps={{isPublic: true, placeholder: 'Write a new public post'}}
-        queueNewMsgs={queueNewMsgs}
+        queueNewMsgs
         dateDividers
         filter={filter}
-        Toolbar={Toolbar}
-        ListItem={ListItem}
+        Hero={Hero}
+        ListItem={Card}
         live={{ gt: [Date.now(), null] }}
         emptyMsg="Your newsfeed is empty."
         append={this.helpCards.bind(this)}
