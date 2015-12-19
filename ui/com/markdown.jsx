2dda7b17b0ed70e13f2f03117e8686a27d2b53b5
Paul Frazee <pfrazee@gmail.com>
2015-12-19 17:57:52 -0600

    ui: add tooltips to links in posts

--- ui/com/markdown.jsx
+++ ui/com/markdown.jsx
@@ -1,10 +1,36 @@
 'use babel'
 import React from 'react'
 import markdown from '../lib/markdown'
+import Drop from 'tether-drop'
 
 export class Block extends React.Component {
+  componentDidMount() {
+    this.drops = Array.from(this.refs.md.querySelectorAll('a')).map(linkEl => {
+      return new Drop({
+        target: linkEl,
+        content: linkEl.getAttribute('href'),
+        openOn: 'hover',
+        hoverOpenDelay: 250,
+        classes: 'drop-theme-tooltip drop-theme-arrows',
+        tetherOptions: {
+          attachment: 'top center',
+          targetAttachment: 'bottom center',
+          constraints: [
+            {
+              to: 'scrollParent',
+              attachment: 'together',
+              pin: true
+            }
+          ]
+        }
+      })
+    })
+  }
+  componentWillUnmount() {
+    this.drops.forEach(drop => drop.destroy())
+  }
   render() {
-    return <div className="markdown markdown-block" dangerouslySetInnerHTML={{__html: markdown.block(this.props.md, this.props.msg)}} />
+    return <div ref="md" className="markdown markdown-block" dangerouslySetInnerHTML={{__html: markdown.block(this.props.md, this.props.msg)}} />
   }
 }
 export class Inline extends React.Component {
