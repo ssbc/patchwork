a90c27231d275f506b9a0961bc88207a0eaf2daf
du5t <du5t@hitono.info>
2015-12-26 16:13:36 -0800

    semi-sensical inline editing (frontend only)
    - no deletion yet
    - and bad classing so looks bad

--- ui/com/composer/index.jsx
+++ ui/com/composer/index.jsx
@@ -37,8 +37,261 @@ class ComposerTextareaFixed extends React.Component {
     return <textarea ref="textarea" {...this.props} onKeyDown={this.onKeyDown.bind(this)} style={{height: this.props.height, overflow: 'auto'}} />
   }
 }
-const ComposerTextareaVerticalFilled = verticalFilled(ComposerTextareaFixed)
+const ComposerTextareaVerticalFilled = verticalFilled(ComposerTextareaFixed);
 
+export class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+
+      // thread info
+      let recps = this.props.recps || []
+    this.isPublic = this.props.isPublic
+    this.threadRoot = null
+    this.threadBranch = null
+    if (this.props.thread) {
+      // public thread?
+      this.isPublic = isThreadPublic(this.props.thread)
+
+        // root and branch links
+        this.threadRoot = getThreadRoot(this.props.thread)
+        this.threadBranch = threadlib.getLastThreadPost(this.props.thread).key
+
+      // extract encryption recipients from thread
+      if (Array.isArray(this.props.thread.value.content.recps)) {
+        recps = mlib.links(this.props.thread.value.content.recps)
+                    .map(function (recp) { return recp.link })
+                    .filter(Boolean)
+      }
+    }
+
+    // setup state (pulling from thread)
+    this.state = {
+      isPreviewing: false,
+      isSending: false,
+      isReply: !!this.props.thread,
+      hasAddedFiles: false, // used to display a warning if a file was added in public mode, then they switch to private
+      addedFileMeta: {}, // map of file hash -> metadata
+      recps: recps,
+      text: ''
+    }
+  }
+
+  hasChannel() {
+    return !!this.getChannel()
+  }
+
+  getChannel() {
+    const channel = (this.props.thread) ? this.props.thread.value.content.channel : this.props.channel
+    if (typeof channel === 'string')
+      return channel.trim()
+      return false
+  }
+
+  onChangeText(e) {
+    this.setState({ text: e.target.value })
+  }
+
+  onAttach() {
+    this.refs.files.click() // trigger file-selector
+  }
+
+  // called by the files selector when files are chosen
+  onFilesAdded() {
+
+    var done = multicb({ pluck: 1 })
+      var filesInput = this.refs.files
+    var handled=0, total = filesInput.files.length
+    this.setState({ isAddingFiles: true, hasAddedFiles: true })
+
+      let add = (f) => {
+        // limit to 5mb
+        if (f.size > 5 * (1024*1024)) {
+          var inMB = Math.round(f.size / (1024*1024) * 100) / 100
+          app.issue('Error Attaching File', new Error(f.name + ' is larger than the 5 megabyte limit (' + inMB + ' MB)'))
+            this.setState({ isAddingFiles: false })
+            return false
+        }
+        // hash file
+        app.ssb.patchwork.addFileToBlobs(f.path, (err, res) => {
+          if (err) {
+            app.issue('Error Attaching File', error, 'This error occurred while trying to add a file to the blobstore for a new post.')
+          } else {
+            // insert the mention
+            var str = ''
+            if (!(/(^|\s)$/.test(this.state.text)))
+              str += ' ' // add some space if not on a newline
+            if (isImageFilename(f.name))
+              str += '!' // inline the image
+            str += '['+(f.name||'untitled')+']('+res.hash+')'
+            this.setState({ text: this.state.text + str })
+
+              // capture metadata
+              var meta = this.state.addedFileMeta[res.hash] = {
+                name: f.name || 'untitled',
+                size: f.size
+              }
+            if (mime.contentType(f.name))
+              meta.type = mime.contentType(f.name)
+              if (res.width)
+              meta.width = res.width
+            if (res.height)
+              meta.height = res.height
+            this.setState({ addedFileMeta: this.state.addedFileMeta })
+          }
+          if (++handled >= total)
+            this.setState({ isAddingFiles: false })
+        })
+          return true
+      }
+
+    // hash the files
+    for (var i=0; i < total; i++) {
+      if (!add(filesInput.files[i]))
+        return false
+    }
+    filesInput.value = '' // clear file list
+  }
+
+  onAddRecp(id) {
+    let recps = this.state.recps
+
+    // enforce limit
+    if (recps.length >= RECP_LIMIT)
+      return
+
+    // remove if already exists (we'll push to end of list so user sees its there)
+    var i = recps.indexOf(id)
+      if (i !== -1)
+      recps.splice(i, 1)
+      recps.push(id)
+      this.setState({ recps: recps })
+  }
+
+  onRemoveRecp(id) {
+    let recps = this.state.recps
+    var i = recps.indexOf(id)
+      if (i !== -1) {
+        recps.splice(i, 1)
+          this.setState({ recps: recps })
+      }
+  }
+
+  canSend() {
+    return !!this.state.text.trim()
+  }
+
+  onSend() {
+    var text = this.state.text
+    if (!text.trim())
+      return
+
+    this.setState({ isSending: true })
+
+      // prep text
+      mentionslib.extract(text, (err, mentions) => {
+        if (err) {
+          this.setState({ isSending: false })
+            if (err.conflict)
+            app.issue('Error While Publishing', 'You follow multiple people with the name "'+err.name+'." Resolve this before publishing.')
+            else
+            app.issue('Error While Publishing', err, 'This error occured while trying to extract the mentions from a new post.')
+            return
+        }
+
+        // add file meta to mentions
+        if (mentions && mentions.length) {
+          mentions.forEach(mention => {
+            var meta = this.state.addedFileMeta[mention.link]
+            if (meta) {
+              for (var k in meta) {
+                if (k != 'link')
+                  mention[k] = meta[k]
+              }
+            }
+          })
+        }
+
+        let recps = null, recpLinks = null
+        if (!this.isPublic) {
+          // setup recipients
+          recps = this.state.recps
+
+          // make sure the user is in the recipients
+          if (recps.indexOf(app.user.id) === -1)
+            recps.push(app.user.id)
+
+            // setup links
+            recpLinks = recps.map((id) => {
+              let name = u.getName(id)
+                return (name) ? { link: id, name: name } : id
+            })
+        }
+
+        // publish
+        var post = schemas.post(text, this.threadRoot, this.threadBranch, mentions, recpLinks, this.getChannel())
+          let published = (err, msg) => {
+            this.setState({ isSending: false })
+              if (err) app.issue('Error While Publishing', err, 'This error occurred while trying to publish a new post.')
+              else {
+                // reset form
+                this.setState({ text: '', isPreviewing: false })
+
+                  // mark read (include the thread root because the api will automatically mark the root unread on new reply)
+                  app.ssb.patchwork.markRead((this.threadRoot) ? [this.threadRoot, msg.key] : msg.key)
+
+                  // call handler
+                  if (this.props.onSend)
+                  this.props.onSend(msg)
+              }
+          }
+        if (recps)
+          app.ssb.private.publish(post, recps, published)
+          else
+          app.ssb.publish(post, published)
+      })
+  }
+
+  render() {
+    const channel = this.getChannel()
+      const setPreviewing = b => () => this.setState({ isPreviewing: b })
+      const ComposerTextarea = (this.props.verticalFilled) ? ComposerTextareaVerticalFilled : ComposerTextareaFixed
+    const Preview = (props) => {
+      return <div>
+        <div className="card" style={{padding: '20px', margin: '40px 10px 30px 0'}}><MarkdownBlock md={this.state.text} /></div>
+      </div>
+    }
+    const sendIcon = (this.isPublic) ? 'users' : 'lock'
+    return <div className="composer">
+        <input ref="files" type="file" multiple onChange={this.onFilesAdded.bind(this)} style={{display: 'none'}} />
+        <Modal Content={Preview} isOpen={this.state.isPreviewing} onClose={setPreviewing(false)} />
+        { channel ? 
+          <div className="composer-channel">#{channel}</div>
+          : '' }
+          <div className="composer-content">
+            <ComposerTextarea ref="textarea" value={this.props.editingContent} onChange={this.onChangeText.bind(this)} onSubmit={this.onSend.bind(this)} placeholder={!this.state.isReply ? this.props.placeholder : 'Write a reply'} />
+          </div>
+          <div className="composer-ctrls flex">
+            <div className="flex-fill">
+              { !this.isPublic ?
+                (this.state.hasAddedFiles ? <em>Warning: attachments don{'\''}t work yet in private messages. Sorry!</em> : '') :
+                (this.state.isAddingFiles ?
+                 <em>Adding...</em> :
+                 <a className="btn" onClick={this.onAttach.bind(this)}><i className="fa fa-paperclip" /> Add an attachment</a>) }
+            </div>
+            <div>
+              <a className="btn" onClick={setPreviewing(true)}>Preview</a>
+            </div>
+            <div>
+              { (!this.canSend() || this.state.isSending) ?
+                <a className="btn disabled">Submit Edit</a> :
+                <a className="btn highlighted" onClick={this.onSend.bind(this)}><i className={`fa fa-${sendIcon}`}/> Submit Edit</a> }
+            </div>
+          </div>
+    </div>
+  }
+  
+}
+  
 export default class Composer extends React.Component {
   constructor(props) {
     super(props)
@@ -267,7 +520,6 @@ export default class Composer extends React.Component {
       { channel ? 
         <div className="composer-channel">#{channel}</div>
         : '' }
-      <ComposerRecps isPublic={this.isPublic} isReadOnly={this.state.isReply} recps={this.state.recps} onAdd={this.onAddRecp.bind(this)} onRemove={this.onRemoveRecp.bind(this)} />
       <div className="composer-content">
         <ComposerTextarea ref="textarea" value={this.state.text} onChange={this.onChangeText.bind(this)} onSubmit={this.onSend.bind(this)} placeholder={!this.state.isReply ? this.props.placeholder : 'Write a reply'} />
       </div>
@@ -284,7 +536,7 @@ export default class Composer extends React.Component {
         </div>
         <div>
           { (!this.canSend() || this.state.isSending) ?
-            <a className="btn disabled">Send</a> :
+            <a className="btn disabled">Submit Edit</a> :
             <a className="btn highlighted" onClick={this.onSend.bind(this)}><i className={`fa fa-${sendIcon}`}/> Send</a> }
         </div>
       </div>
--- ui/com/msg-view/card.jsx
+++ ui/com/msg-view/card.jsx
@@ -12,6 +12,7 @@ import { Inline as MdInline } from '../markdown'
 import Modal from '../modals/single'
 import FlagMsgForm from '../forms/flag-msg'
 import { countReplies } from '../../lib/msg-relation'
+import { Editor } from '../composer/index'
 import DropdownBtn from '../dropdown'
 import u from '../../lib/util'
 import app from '../../lib/app'
@@ -65,6 +66,7 @@ export default class Card extends React.Component {
       isOversized: false,
       isExpanded: false,
       isViewingRaw: false,
+      isEditing: false,
       subject: null,
       wasLinkCopied: null, // used by the link-copy behavior to give confirmation
       isFlagModalOpen: false
@@ -98,8 +100,10 @@ export default class Card extends React.Component {
       this.props.onFlag(this.props.msg, 'unflag');
     else if (choice === 'toggle-raw')
       this.setState({ isViewingRaw: !this.state.isViewingRaw });
-    else if (choice === 'edit-post')
+    else if (choice === 'edit-post') {
       console.log("trying to edit post");
+      this.setState({ isEditing: !this.state.isEditing });
+    }
   }
 
   copyLink() {
@@ -115,7 +119,7 @@ export default class Card extends React.Component {
 
   componentDidMount() {
     // load subject msg, if needed
-    let msg = this.props.msg
+    let msg = this.props.msg;
     if (msg.value && msg.value.content.type === 'vote') {
       let vote = mlib.link(msg.value.content.vote, 'msg')
       if (vote) {
@@ -129,20 +133,19 @@ export default class Card extends React.Component {
         })
       }
     }
-
     // is the card oversized?
     if (!this.refs.body)
       return
     // wait for images to finish loading
     var done = multicb()
-    Array.from(this.refs.body.querySelectorAll('img')).forEach(el => onImageLoaded(el, done()))
+    Array.from(this.refs.body.querySelectorAll('img')).forEach(el => onImageLoaded(el, done()));
     done(() => {
       // check height
       if (!this.refs.body)
-        return
-      const rect = this.refs.body.getClientRects()[0]
+        return;
+      const rect = this.refs.body.getClientRects()[0];
       if (rect && rect.height > MAX_CONTENT_HEIGHT) {
-        this.setState({ isOversized: true })
+        this.setState({ isOversized: true });
       }
     })
   }
@@ -153,19 +156,26 @@ export default class Card extends React.Component {
   }
 
   render() {
+    console.log('msg render called');
+
     const msg = this.props.msg
     if (msg.isNotFound)
-      return this.renderNotFound(msg)
+      return this.renderNotFound(msg);
     if (msg.isLink)
-      return this.renderLink(msg)
+      return this.renderLink(msg);
     if (msg.isMention)
-      return this.renderMention(msg)
-    const upvoters = getVotes(this.props.msg, userId => msg.votes[userId] === 1)
-    const downvoters = getVotes(this.props.msg, userId => userIsTrusted(userId) && msg.votes[userId] === -1)
-    const isUpvoted = upvoters.indexOf(app.user.id) !== -1
-    const isDownvoted = downvoters.indexOf(app.user.id) !== -1
+      return this.renderMention(msg);
+
+    const upvoters = getVotes(this.props.msg, userId => msg.votes[userId] === 1);
+    const downvoters = getVotes(this.props.msg, userId => userIsTrusted(userId) && msg.votes[userId] === -1);
+    const isUpvoted = upvoters.indexOf(app.user.id) !== -1;
+    const isDownvoted = downvoters.indexOf(app.user.id) !== -1;
+
+    if (this.state.isEditing)
+      return this.renderEditor(msg, upvoters, downvoters, isUpvoted, isDownvoted);
+
     if (msg.value.content.type == 'post' && downvoters.length > upvoters.length && !this.state.isExpanded)
-      return this.renderMuted(msg)
+      return this.renderMuted(msg);
     return this.renderPost(msg, upvoters, downvoters, isUpvoted, isDownvoted)
   }
 
@@ -223,7 +233,7 @@ export default class Card extends React.Component {
     const channel = msg && msg.value && msg.value.content && msg.value.content.channel
 
     const dropdownOpts = [
-      { value: 'edit-post',  label: <span><i className="fa fa-edit-post" /> Edit/Delete Post</span> },
+      { value: 'edit-post',  label: <span><i className="fa fa-pencil" /> Edit/Delete Post</span> },
       { value: 'copy-link',  label: <span><i className="fa fa-external-link" /> Copy Link</span> },
       { value: 'toggle-raw', label: <span><i className={isViewingRaw?'fa fa-envelope-o':'fa fa-gears'} /> View {isViewingRaw?'Msg':'Data'}</span> },
       (isDownvoted) ? 
@@ -274,5 +284,66 @@ export default class Card extends React.Component {
       </div>
       <Modal isOpen={this.state.isFlagModalOpen} onClose={this.onCloseFlagModal.bind(this)} Form={FlagMsgForm} formProps={{msg: msg, onSubmit: this.onSubmitFlag.bind(this)}} nextLabel="Publish" />
     </div>
+  }
+
+  renderEditor(msg, upvoters, downvoters, isUpvoted, isDownvoted) {
+    const replies = countReplies(msg);
+    const unreadReplies = countReplies(msg, m => !m.isRead);
+    const isViewingRaw = this.state.isViewingRaw;
+    const channel = msg && msg.value && msg.value.content && msg.value.content.channel;
+
+    const dropdownOpts = [
+      { value: 'edit-post',  label: <span><i className="fa fa-pencil" /> Edit/Delete Post</span> },
+      { value: 'copy-link',  label: <span><i className="fa fa-external-link" /> Copy Link</span> },
+      { value: 'toggle-raw', label: <span><i className={isViewingRaw?'fa fa-envelope-o':'fa fa-gears'} /> View {isViewingRaw?'Msg':'Data'}</span> },
+      (isDownvoted) ? 
+      { value: 'unflag',   label: <span><i className="fa fa-times" /> Unflag</span> } :
+      { value: 'flag',     label: <span><i className="fa fa-flag" /> Flag</span> }
+    ];
+
+    const oversizedCls = (this.state.isOversized?'oversized':'');
+    const expandedCls  = (this.state.isExpanded?'expanded':'');
+    const newCls       = (msg.isNew?'new':'');
+    return <div className={`msg-view card-post ${oversizedCls} ${expandedCls} ${newCls}`}>
+      <div className="left-meta">
+        <UserPic id={msg.value.author} />
+        <div><a onClick={this.onSelect.bind(this)}><NiceDate ts={msg.value.timestamp} /></a></div>
+      </div>
+      <div className="content">
+        <div className="header">
+          <div className="header-left">
+            <UserLink id={msg.value.author} />{' '}
+            {msg.plaintext ? '' : <i className="fa fa-lock"/>}{' '}
+            {msg.mentionsUser ? <i className="fa fa-at"/> : ''}{' '}
+            {channel ? <span className="channel">in <Link to={`/newsfeed/channel/${channel}`}>#{channel}</Link></span> : ''}
+          </div>
+          <div className="header-right">
+            { this.state.wasLinkCopied ? <small>Copied!</small> : '' }
+            { !this.props.noBookmark ? <BookmarkBtn isBookmarked={msg.isBookmarked} onClick={()=>this.props.onToggleBookmark(msg)} /> : '' }
+            <DropdownBtn items={dropdownOpts} right onSelect={this.onSelectDropdown.bind(this)}><i className="fa fa-ellipsis-h" /></DropdownBtn>
+          </div>
+        </div>
+        <div className="body" ref="body">
+          { /* <Content msg={msg} forceRaw={isViewingRaw||this.props.forceRaw} /> */ }
+          <Editor isEditing="true" editingContent={msg.value.content.text} />
+          
+        </div>
+        <div className="ctrls">
+          { replies && !this.props.noReplies ?
+            <div>
+              <a onClick={this.onSelect.bind(this)}>
+                {replies === 1 ? '1 reply ' : (replies + ' replies ')}
+                { unreadReplies ? <strong>{unreadReplies} new</strong> : '' }
+              </a>
+            </div> : '' }
+            { upvoters.length ? <div className="upvoters flex-fill"><i className="fa fa-hand-peace-o"/> by <UserLinks ids={upvoters}/></div> : ''}
+            { downvoters.length ? <div className="downvoters flex-fill"><i className="fa fa-flag"/> by <UserLinks ids={downvoters}/></div> : ''}
+            { !upvoters.length && !downvoters.length ? <div className="flex-fill" /> : '' }
+            <div><DigBtn onClick={()=>this.props.onToggleStar(msg)} isUpvoted={isUpvoted} /></div>
+            { !this.props.noReplies ? <div><a onClick={this.onSelect.bind(this)}><i className="fa fa-reply" /> Reply</a></div> : '' }
+        </div>
+      </div>
+      <Modal isOpen={this.state.isFlagModalOpen} onClose={this.onCloseFlagModal.bind(this)} Form={FlagMsgForm} formProps={{msg: msg, onSubmit: this.onSubmitFlag.bind(this)}} nextLabel="Publish" />
+      </div>
   }
 }
