a3d86277fa45edc207afeda52be9e774077c4bd2
Paul Frazee <pfrazee@gmail.com>
2015-12-27 17:00:49 -0700

    ui: add leftnav to msg page

--- ui/views/msg.jsx
+++ ui/views/msg.jsx
@@ -2,16 +2,20 @@
 import React from 'react'
 import Thread from '../com/msg-thread'
 import mlib from 'ssb-msgs'
+import LeftNav from '../com/leftnav'
 import app from '../lib/app'
 import u from '../lib/util'
 
 export default class Msg extends React.Component {
   render() {
     const id = this.props.params && this.props.params.id
-    return <div id="msg">
-      { id
-        ? <Thread id={id} live />
-        : <div style={{padding: 20, fontWeight: 300, textAlign:'center'}}>No thread selected.</div> }
+    return <div id="msg" className="flex">
+      <LeftNav location={this.props.location} />
+      <div className="flex-fill">
+        { id
+          ? <Thread id={id} live />
+          : <div style={{padding: 20, fontWeight: 300, textAlign:'center'}}>No thread selected.</div> }
+      </div>
     </div>
   }
 }
