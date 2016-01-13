d4d759ce89318b80c572c4d49709847bc73f2de7
Charles Lehner <cel@celehner.com>
2016-01-13 13:11:49 -0500

    Add live-only stream option to API

--- api/index.js
+++ api/index.js
@@ -524,6 +524,10 @@ exports.init = function (sbot, opts) {
       var readPush = pushable()
       var read = pull(readPush, paramap(fetch))
 
+      // allow live-only stream
+      if (opts && opts.live === 'only')
+        readPush.end()
+      else
       // await sync, then emit the reads
       awaitSync(function () {
         var added = 0
