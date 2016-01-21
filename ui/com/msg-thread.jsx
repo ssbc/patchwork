0b25db67a4c9bee72bd89dc89faaf85ecd24367e
du5t <du5t@hitono.info>
2016-01-20 20:33:38 -0800

    reverted msg-thread edits

--- ui/com/msg-thread.jsx
+++ ui/com/msg-thread.jsx
@@ -17,9 +17,9 @@ class BookmarkBtn extends React.Component {
   render() {
     const b = this.props.isBookmarked
     const title = 'Bookmark'+(b?'ed':'')
-      return <a className={(b?' selected':'')} onClick={this.props.onClick} title={title}>
+    return <a className={(b?' selected':'')} onClick={this.props.onClick} title={title}>
         <i className={'fa fa-bookmark'+(b?'':'-o')} /> {title}
-      </a>
+    </a>
   }
 }
 
@@ -27,21 +27,21 @@ class UnreadBtn extends React.Component {
   render() {
     const b = this.props.isUnread
     const title = (b?'Mark Read':'Mark Unread')
-      const icon = 'fa fa-'+(b?'square-o':'check-square-o')
-      return <a onClick={this.props.onClick} title={title}>
-        <i className={icon} /> {title}
-      </a>
+    const icon = 'fa fa-'+(b?'square-o':'check-square-o')
+    return <a onClick={this.props.onClick} title={title}>
+      <i className={icon} /> {title}
+    </a>
   }
 }
-
+            
 
 export default class Thread extends React.Component {
   constructor(props) {
     super(props)
-      this.state = {
-        thread: null,
-        msgs: []
-      }
+    this.state = {
+      thread: null,
+      msgs: []
+    }
     this.liveStream = null
   }
 
@@ -52,54 +52,54 @@ export default class Thread extends React.Component {
       if (err)
         return app.issue('Failed to Load Message', err, 'This happened in msg-list componentDidMount')
 
-        // set state, after some processing
-        this.setState({
-          thread: thread,
-          msgs: threadlib.flattenThread(thread),
-          isReplying: (this.state.thread && thread.key === this.state.thread.key) ? this.state.isReplying : false
-        })
+      // set state, after some processing
+      this.setState({
+        thread: thread,
+        msgs: threadlib.flattenThread(thread),
+        isReplying: (this.state.thread && thread.key === this.state.thread.key) ? this.state.isReplying : false
+      })
 
-        // mark read
-        if (thread.hasUnread) {
-          threadlib.markThreadRead(app.ssb, thread, (err) => {
-            if (err)
-              return app.minorIssue('Failed to mark thread as read', err)
-              this.setState({ thread: thread })
-          })
-        }
+      // mark read
+      if (thread.hasUnread) {
+        threadlib.markThreadRead(app.ssb, thread, (err) => {
+          if (err)
+            return app.minorIssue('Failed to mark thread as read', err)
+          this.setState({ thread: thread })
+        })
+      }
 
       // listen for new replies
       if (this.props.live) {
         if (this.liveStream)
           this.liveStream(true, ()=>{}) // abort existing livestream
 
-          pull(
-            // listen for all new messages
-            (this.liveStream = app.ssb.createLogStream({ live: true, gt: Date.now() })),
-            pull.filter(obj => !obj.sync), // filter out the sync obj
-            pull.asyncMap((msg, cb) => threadlib.decryptThread(app.ssb, msg, cb)),
-            pull.drain((msg) => {
-              if (!this.state.thread)
-                return
-              
-              var c = msg.value.content
-              var rels = mlib.relationsTo(msg, this.state.thread)
-                // reply post to this thread?
-                if (c.type == 'post' && (rels.indexOf('root') >= 0 || rels.indexOf('branch') >= 0)) {
-                  // add to thread and flatlist
-                  this.state.msgs.push(msg)
-                    this.state.thread.related = (this.state.thread.related||[]).concat(msg)
-                    this.setState({ thread: this.state.thread, msgs: this.state.msgs })
+        pull(
+          // listen for all new messages
+          (this.liveStream = app.ssb.createLogStream({ live: true, gt: Date.now() })),
+          pull.filter(obj => !obj.sync), // filter out the sync obj
+          pull.asyncMap((msg, cb) => threadlib.decryptThread(app.ssb, msg, cb)),
+          pull.drain((msg) => {
+            if (!this.state.thread)
+              return
+            
+            var c = msg.value.content
+            var rels = mlib.relationsTo(msg, this.state.thread)
+            // reply post to this thread?
+            if (c.type == 'post' && (rels.indexOf('root') >= 0 || rels.indexOf('branch') >= 0)) {
+              // add to thread and flatlist
+              this.state.msgs.push(msg)
+              this.state.thread.related = (this.state.thread.related||[]).concat(msg)
+              this.setState({ thread: this.state.thread, msgs: this.state.msgs })
 
-                    // mark read
-                    thread.hasUnread = true
-                  threadlib.markThreadRead(app.ssb, this.state.thread, err => {
-                    if (err)
-                      app.minorIssue('Failed to mark live-streamed reply as read', err)
-                  })
-                }
-            })
-          )
+              // mark read
+              thread.hasUnread = true
+              threadlib.markThreadRead(app.ssb, this.state.thread, err => {
+                if (err)
+                  app.minorIssue('Failed to mark live-streamed reply as read', err)
+              })
+            }
+          })
+        )
       }
     })
   }
@@ -122,8 +122,8 @@ export default class Thread extends React.Component {
       if (err)
         return app.minorIssue('Failed to mark unread', err, 'Happened in onMarkUnread of MsgThread')
 
-        // re-render
-        thread.isRead = isRead
+      // re-render
+      thread.isRead = isRead
       thread.hasUnread = !isRead
       this.setState(this.state)
     })
@@ -135,8 +135,8 @@ export default class Thread extends React.Component {
       if (err)
         return app.issue('Failed to toggle bookmark', err, 'Happened in onToggleBookmark of MsgThread')
 
-        // re-render
-        msg.isBookmarked = isBookmarked
+      // re-render
+      msg.isBookmarked = isBookmarked
       this.setState(this.state)
     })
   }
@@ -149,45 +149,45 @@ export default class Thread extends React.Component {
 
     // publish new message
     var voteMsg = schemas.vote(msg.key, newVote)
-      let done = (err) => {
-        if (err)
-          return app.issue('Failed to publish vote', err, 'Happened in onToggleStar of MsgThread')
+    let done = (err) => {
+      if (err)
+        return app.issue('Failed to publish vote', err, 'Happened in onToggleStar of MsgThread')
 
-          // re-render
-          msg.votes[app.user.id] = newVote
-        this.setState(this.state)
-      }
+      // re-render
+      msg.votes[app.user.id] = newVote
+      this.setState(this.state)
+    }
     if (msg.plaintext)
       app.ssb.publish(voteMsg, done)
-      else {
-        let recps = mlib.links(msg.value.content.recps).map(l => l.link)
-          app.ssb.private.publish(voteMsg, recps, done)
-      }
+    else {
+      let recps = mlib.links(msg.value.content.recps).map(l => l.link)
+      app.ssb.private.publish(voteMsg, recps, done)
+    }
   }
 
   onFlag(msg, reason) {
     if (!reason)
       throw new Error('reason is required')
 
-      // publish new message
-      const voteMsg = (reason === 'unflag') // special case
+    // publish new message
+    const voteMsg = (reason === 'unflag') // special case
       ? schemas.vote(msg.key, 0)
       : schemas.vote(msg.key, -1, reason)
-      let done = (err) => {
-        if (err)
-          return app.issue('Failed to publish flag', err, 'Happened in onFlag of MsgThread')
+    let done = (err) => {
+      if (err)
+        return app.issue('Failed to publish flag', err, 'Happened in onFlag of MsgThread')
 
-          // re-render
-          msg.votes = msg.votes || {}
-        msg.votes[app.user.id] = (reason === 'unflag') ? 0 : -1
-        this.setState(this.state)
-      }
+      // re-render
+      msg.votes = msg.votes || {}
+      msg.votes[app.user.id] = (reason === 'unflag') ? 0 : -1
+      this.setState(this.state)
+    }
     if (msg.plaintext)
       app.ssb.publish(voteMsg, done)
-      else {
-        let recps = mlib.links(msg.value.content.recps).map(l => l.link)
-          app.ssb.private.publish(voteMsg, recps, done)
-      }
+    else {
+      let recps = mlib.links(msg.value.content.recps).map(l => l.link)
+      app.ssb.private.publish(voteMsg, recps, done)
+    }
   }
 
   onSend(msg) {
@@ -202,7 +202,7 @@ export default class Thread extends React.Component {
   onSelectRoot() {
     let thread = this.state.thread
     let threadRoot = mlib.link(thread.value.content.root, 'msg')
-      this.openMsg(threadRoot.link)
+    this.openMsg(threadRoot.link)
   }
 
   onCloseMsg() {
@@ -212,53 +212,53 @@ export default class Thread extends React.Component {
   render() {
     const thread = this.state.thread
     const threadRoot = thread && mlib.link(thread.value.content.root, 'msg')
-      const canMarkUnread = thread && (thread.isBookmarked || !thread.plaintext)
-      const isPublic = (thread && thread.plaintext)
-      const authorName = thread && u.getName(thread.value.author)
-      const recps = thread && mlib.links(thread.value.content.recps, 'feed')
-      return <div className="msg-thread">
-        <VerticalFilledContainer id="msg-thread-vertical" className="flex">
-          <LeftNav location={this.props.location} />
-          <div className="flex-fill" style={{padding: 5}}>
-            { !thread
-              ? <div style={{padding: 20, fontWeight: 300, textAlign:'center'}}>No thread selected.</div>
-              : <ResponsiveElement widthStep={250}>
-            <div className="flex light-toolbar">
-              { threadRoot
-                ? <a onClick={this.onSelectRoot.bind(this)}><i className="fa fa-angle-double-up" /> Parent Thread</a>
-                : '' }
-              { !threadRoot && thread
-                ? <BookmarkBtn onClick={()=>this.onToggleBookmark(thread)} isBookmarked={thread.isBookmarked} />
-                : '' }
-              { thread
-                ? <UnreadBtn onClick={this.onToggleUnread.bind(this)} isUnread={thread.hasUnread} />
-                : '' }
-            </div>
-            <hr className="labeled" data-label={`${isPublic?'Public':'Private'} post by ${authorName}${isPublic?'':' to:'}`} />
-            { recps && recps.length
-              ? <div className="recps-list flex"><div className="flex-fill"/><UserPics ids={recps.map(r => r.link)} /><div className="flex-fill"/></div>
-              : '' }
-            <ReactCSSTransitionGroup component="div" className="items" transitionName="fade" transitionAppear={true} transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={1}>
-              { this.state.msgs.map((msg, i) => {
-                  const isFirst = (i === 0)
+    const canMarkUnread = thread && (thread.isBookmarked || !thread.plaintext)
+    const isPublic = (thread && thread.plaintext)
+    const authorName = thread && u.getName(thread.value.author)
+    const recps = thread && mlib.links(thread.value.content.recps, 'feed')
+    return <div className="msg-thread">
+      <VerticalFilledContainer id="msg-thread-vertical" className="flex">
+        <LeftNav location={this.props.location} />
+        <div className="flex-fill" style={{padding: 5}}>
+          { !thread
+            ? <div style={{padding: 20, fontWeight: 300, textAlign:'center'}}>No thread selected.</div>
+            : <ResponsiveElement widthStep={250}>
+                <div className="flex light-toolbar">
+                  { threadRoot
+                    ? <a onClick={this.onSelectRoot.bind(this)}><i className="fa fa-angle-double-up" /> Parent Thread</a>
+                    : '' }
+                  { !threadRoot && thread
+                    ? <BookmarkBtn onClick={()=>this.onToggleBookmark(thread)} isBookmarked={thread.isBookmarked} />
+                    : '' }
+                  { thread
+                    ? <UnreadBtn onClick={this.onToggleUnread.bind(this)} isUnread={thread.hasUnread} />
+                    : '' }
+                </div>
+                <hr className="labeled" data-label={`${isPublic?'Public':'Private'} post by ${authorName}${isPublic?'':' to:'}`} />
+                { recps && recps.length
+                  ? <div className="recps-list flex"><div className="flex-fill"/><UserPics ids={recps.map(r => r.link)} /><div className="flex-fill"/></div>
+                  : '' }
+                <ReactCSSTransitionGroup component="div" className="items" transitionName="fade" transitionAppear={true} transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={1}>
+                  { this.state.msgs.map((msg, i) => {
+                    const isFirst = (i === 0)
                     return <Card
-                               key={msg.key}
-                               msg={msg}
-                               noReplies
-                               noBookmark
-                               forceRaw={this.props.forceRaw}
-                               forceOpen={isFirst}
-                               onSelect={()=>this.openMsg(msg.key)}
-                               onToggleStar={()=>this.onToggleStar(msg)}
-                               onFlag={(msg, reason)=>this.onFlag(msg, reason)}
-                               onToggleBookmark={()=>this.onToggleBookmark(msg)} />
-                }) }
-              <div key="composer" className="container"><Composer key={thread.key} thread={thread} onSend={this.onSend.bind(this)} /></div>
-            </ReactCSSTransitionGroup>
+                      key={msg.key}
+                      msg={msg}
+                      noReplies
+                      noBookmark
+                      forceRaw={this.props.forceRaw}
+                      forceOpen={isFirst}
+                      onSelect={()=>this.openMsg(msg.key)}
+                      onToggleStar={()=>this.onToggleStar(msg)}
+                      onFlag={(msg, reason)=>this.onFlag(msg, reason)}
+                      onToggleBookmark={()=>this.onToggleBookmark(msg)} />
+                  }) }
+                  <div key="composer" className="container"><Composer key={thread.key} thread={thread} onSend={this.onSend.bind(this)} /></div>
+                </ReactCSSTransitionGroup>
               </ResponsiveElement>
-            }
-          </div>
-        </VerticalFilledContainer>
-      </div>
+          }
+        </div>
+      </VerticalFilledContainer>
+    </div>
   }
 }
