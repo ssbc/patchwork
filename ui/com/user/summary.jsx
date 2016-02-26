1ba6be8cbebe6542225a2b89a098e45ec0c77b44
Paul Frazee <pfrazee@gmail.com>
2016-02-26 16:38:07 -0600

    more work on the user-summaries

--- ui/com/user/summary.jsx
+++ ui/com/user/summary.jsx
@@ -12,7 +12,8 @@ export default class UserSummary extends AutoRefreshingComponent {
     const pid = props.pid
     return {
       name:       app.users.names[pid] || u.shortString(pid, 6),
-      following:  social.follows(app.user.id, pid)
+      following:  social.follows(app.user.id, pid),
+      follower:   social.follows(pid, app.user.id)
     }
   }
 
@@ -34,23 +35,25 @@ export default class UserSummary extends AutoRefreshingComponent {
   render() {
     return <div className="user-summary" onClick={this.onClick.bind(this)}>
       <UserPic id={this.props.pid} />
-      <div className="user-name">{ this.state.name }</div>
-      { this.props['follow-btn'] && this.props.pid !== app.user.id
-        ? (this.state.following
-          ? <a className="btn follow-btn" onClick={this.onToggleFollow.bind(this)}>
-              <HoverShifter>
-                <span><i className="fa fa-check" /></span>
-                <span><i className="fa fa-times" /> Unfollow</span>
-              </HoverShifter>
-            </a>
-          : <a className="btn follow-btn" onClick={this.onToggleFollow.bind(this)}>
-              <HoverShifter>
-                <span><i className="fa fa-plus" /></span>
-                <span><i className="fa fa-plus" /> Follow</span>
-              </HoverShifter>
-            </a>)
-        : ''
-      }
+      <div className="user-name">
+        <div>{ this.state.name }</div>
+        { this.props['follow-btn'] && this.props.pid !== app.user.id
+          ? (this.state.following
+            ? <a className="btn follow-btn unfollow" onClick={this.onToggleFollow.bind(this)}>
+                <HoverShifter>
+                  <span><i className="fa fa-check" /> { this.state.follower ? 'Friend' : 'Following' }</span>
+                  <span><i className="fa fa-times" /> Unfollow</span>
+                </HoverShifter>
+              </a>
+            : <a className="btn follow-btn" onClick={this.onToggleFollow.bind(this)}>
+                <HoverShifter>
+                  <span><i className="fa fa-plus" /> Follow</span>
+                  <span><i className="fa fa-plus" /> Follow</span>
+                </HoverShifter>
+              </a>)
+          : ''
+        }
+      </div>
     </div>
   }
 }
@@ -68,9 +71,9 @@ export class UserSummaries extends React.Component {
       { groups.map(ids => {
         return <div className="user-summaries-row">{ ids.map(id => {
           if (id.addContactBtn)
-            return <a className="user-add hint--top" data-hint="Add Friends" href='#/add-contact'><i className="fa fa-user-plus" /></a>
+            return <a className="user-add hint--bottom" data-hint="Add Friends" href='#/add-contact'><i className="fa fa-user-plus" /></a>
           if (id.joinPubBtn)
-            return <a className="user-add hint--top" data-hint="Join a Pub" onClick={this.props.onClickJoinPub}><i className="fa fa-laptop" /></a>
+            return <a className="user-add hint--bottom" data-hint="Join a Pub" onClick={this.props.onClickJoinPub}><i className="fa fa-laptop" /></a>
           if (id.placeholder)
             return <div className="user-summary placeholder" />
           return <UserSummary key={id} pid={id} follow-btn />
--- ui/views/contacts.jsx
+++ ui/views/contacts.jsx
@@ -13,8 +13,7 @@ export default class Contacts extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      contacts: [],
-      pendings: []
+      contacts: []
     }
   }
 
@@ -27,22 +26,17 @@ export default class Contacts extends React.Component {
           return app.minorIssue('An error occurred while fetching known contacts', err)
 
         var contacts = users.filter(id => {
-          // remove non-contacts
-          return social.follows(id, app.user.id) && id !== app.user.id
+          // remove self
+          return id !== app.user.id
         })
         contacts.sort(function (a, b) {
+          // sort alphabetically
           return u.getName(a).localeCompare(u.getName(b))
         })
-        var pendings = users.filter(id => {
-          // remove contacts
-          return !social.follows(id, app.user.id) && id !== app.user.id
-        })
 
-        // add the user and an adder button to the contacts list
-        contacts.unshift(app.user.id)
+        // add the adder button to the contacts list
         contacts.unshift({ addContactBtn: true })
-        
-        this.setState({ contacts, pendings })
+        this.setState({ contacts })
       })
     )
   }
@@ -60,12 +54,6 @@ export default class Contacts extends React.Component {
       <LeftNav location={this.props.location} />
       <div className="flex-fill">
         <div className="user-summaries">
-          { this.state.pendings.length > 0
-            ? <div className="pending">
-              <h2>Following</h2>
-              { this.state.pendings.map(id => <UserPic key={id} id={id} />) }
-            </div>
-            : '' }
           <UserSummaries ids={this.state.contacts} />
         </div>
       </div>
