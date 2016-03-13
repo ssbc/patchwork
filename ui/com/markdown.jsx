61968437564818a2ea9d06c7ca52d1f4798d4c7d
Paul Frazee <pfrazee@gmail.com>
2016-03-12 18:18:47 -0600

    update ssb-markdown usage

--- ui/com/markdown.jsx
+++ ui/com/markdown.jsx
@@ -2,31 +2,40 @@
 import React from 'react'
 import markdown from 'ssb-markdown'
 import ssbref from 'ssb-ref'
-
-function ssbRefToUrl (ref) {
-  if (ssbref.isFeedId(ref))
-    return '#/profile/'+encodeURIComponent(ref)
-  else if (ssbref.isMsgId(ref))
-    return '#/msg/'+encodeURIComponent(ref)
-  else if (ssbref.isBlobId(ref))
-    return 'http://localhost:7778/'+encodeURIComponent(ref)
-  return ''
-}
+import mlib from 'ssb-msgs'
 
 export class Block extends React.Component {
   render() {
+    // extract mention names
+    var mentionNames = {}
+    mlib.links(this.props.msg.value.content.mentions, 'feed').forEach(link => {
+      if (link.name && typeof link.name == 'string') {
+        var name = (link.name.charAt(0) == '@') ? link.name : '@'+link.name
+        mentionNames[name] = link.link
+      }
+    })
+
     const opts = {
-      mentionNames: this.props.msg,
-      ssbRefToUrl
+      toUrl: ref => {
+        // mentions
+        if (ref in mentionNames)
+          return '#/profile/'+encodeURIComponent(mentionNames[ref])
+
+        // standard ssb-refs
+        if (ssbref.isFeedId(ref))
+          return '#/profile/'+encodeURIComponent(ref)
+        else if (ssbref.isMsgId(ref))
+          return '#/msg/'+encodeURIComponent(ref)
+        else if (ssbref.isBlobId(ref))
+          return 'http://localhost:7778/'+encodeURIComponent(ref)
+        return false
+      }
     }
     return <div className="markdown markdown-block" dangerouslySetInnerHTML={{__html: markdown.block(this.props.md, opts)}} />
   }
 }
 export class Inline extends React.Component {
   render() {
-    const opts = {
-      ssbRefToUrl
-    }
-    return <span className="markdown markdown-inline" dangerouslySetInnerHTML={{__html: markdown.inline(this.props.md, opts)}} />
+    return <span className="markdown markdown-inline" dangerouslySetInnerHTML={{__html: markdown.inline(this.props.md)}} />
   }
 }
