aeb64ef804d9d89d3664419052ca631bf516c9ec
Matt McKegg <matt@wetsand.co.nz>
2016-10-28 02:44:07 +1300

    style tweaks

--- styles/index.js
+++ styles/index.js
@@ -4,16 +4,17 @@ var compile = require('micro-css')
 var result = ''
 var additional = ''
 
+additional += fs.readFileSync(require.resolve('patchbay/style.css'), 'utf8')
+
 fs.readdirSync(__dirname).forEach(function (file) {
   if (/\.mcss$/i.test(file)) {
     result += fs.readFileSync(path.resolve(__dirname, file), 'utf8') + '\n'
   }
 
   if (/\.css$/i.test(file)) {
+    console.log(file)
     additional += fs.readFileSync(path.resolve(__dirname, file), 'utf8') + '\n'
   }
 })
-
-additional += fs.readFileSync(require.resolve('patchbay/style.css'), 'utf8')
 
 module.exports = compile(result) + additional
