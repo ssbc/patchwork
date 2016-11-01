13be5ee9906beb5d8c2a24db46c68ef183c78d3a
Matt McKegg <matt@wetsand.co.nz>
2016-11-01 23:55:27 +1300

    poll for new local peers and update interface on discovery

--- modules/obs-local.js
+++ modules/obs-local.js
@@ -3,16 +3,28 @@ var plugs = require('patchbay/plugs')
 var sbot_gossip_peers = plugs.first(exports.sbot_gossip_peers = [])
 var ip = require('ip')
 
+var cache = null
+
 exports.obs_local = function () {
-  var result = MutantSet([], {nextTick: true})
-  sbot_gossip_peers((err, peers) => {
-    if (err) throw console.log(err)
-    peers.filter((peer) => {
-      if (ip.isPrivate(peer.host) && (peer.source === 'local')) {
-        console.log(peer)
-        result.add(peer.key)
-      }
-    })
-  })
-  return result
+  if (cache) {
+    return cache
+  } else {
+    var result = MutantSet([], {nextTick: true})
+    // todo: make this clean up on unlisten
+
+    setTimeout(() => {
+      sbot_gossip_peers((err, peers) => {
+        if (err) throw console.log(err)
+        peers.filter((peer) => {
+          if (ip.isPrivate(peer.host) && (peer.source === 'local')) {
+            console.log(peer)
+            result.add(peer.key)
+          }
+        })
+      })
+    }, 5000)
+
+    cache = result
+    return result
+  }
 }
