9fdc650f04f0bea7982659a5d5de3ce799b8ad6a
Matt McKegg <matt@wetsand.co.nz>
2016-11-03 10:55:56 +1300

    about message: show name instead of id when only specifying a picture

---
+++ modules/about.js
@@ -0,0 +1,37 @@
+var h = require('hyperscript')
+
+function idLink (id) {
+  return h('a', {href:'#'+id}, id.slice(0, 10))
+}
+
+function asLink (ln) {
+  return 'string' === typeof ln ? ln : ln.link
+}
+
+var plugs = require('patchbay/plugs')
+var blob_url = plugs.first(exports.blob_url = [])
+var avatar_name = plugs.first(exports.avatar_name = [])
+var avatar_link = plugs.first(exports.avatar_link = [])
+
+exports.message_content = function (msg) {
+  if(msg.value.content.type !== 'about') return
+
+  if(!msg.value.content.image && !msg.value.content.name)
+    return
+
+  var about = msg.value.content
+  var id = msg.value.content.about
+  return h('p',
+    about.about === msg.value.author
+      ? h('span', 'self-identifies ')
+      : h('span', 'identifies ', about.name ? idLink(id) : avatar_link(id, avatar_name(id))),
+    ' as ',
+    h('a', {href:"#"+about.about},
+      about.name || null,
+      about.image
+      ? h('img.avatar--fullsize', {src: blob_url(about.image)})
+      : null
+    )
+  )
+
+}
