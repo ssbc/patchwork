f8a3919796796fb604a4d98decee02f962ee7cee
Matt McKegg <matt@wetsand.co.nz>
2016-11-04 04:23:31 +1300

    fix connecting to local peers - you now stay permanently connected

--- server-process.js
+++ server-process.js
@@ -25,23 +25,5 @@ module.exports = function (ssbConfig) {
   ssbConfig.manifest = context.sbot.getManifest()
   serveBlobs(context)
   fs.writeFileSync(Path.join(ssbConfig.path, 'manifest.json'), JSON.stringify(ssbConfig.manifest))
-  connectToFriendlyPubs(context.sbot)
   electron.ipcRenderer.send('server-started', ssbConfig)
-}
-
-function connectToFriendlyPubs (sbot) {
-  sbot.gossip.peers((err, peers) => {
-    if (err) return console.log(err)
-    sbot.friends.all((err, friends) => {
-      if (err) return console.log(err)
-      console.log('Attempting to connect to friendly pubs...')
-      peers.filter((p) => p.state !== 'connected' && isFriends(friends, sbot.id, p.key)).forEach((peer) => {
-        sbot.gossip.connect(peer, () => {})
-      })
-    })
-  })
-}
-
-function isFriends (friends, a, b) {
-  return friends[a] && friends[b] && friends[a][b] && friends[b][a]
 }
