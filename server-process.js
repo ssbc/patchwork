36cd0eeba48657072d1b4544e72acf7e5eead61f
Matt McKegg <matt@wetsand.co.nz>
2016-11-04 10:53:34 +1300

    fix prioritizing connection to pubs that are friends

--- server-process.js
+++ server-process.js
@@ -6,7 +6,7 @@ var electron = require('electron')
 var createSbot = require('scuttlebot')
   .use(require('scuttlebot/plugins/master'))
   .use(require('./lib/persistent-gossip')) // override
-  .use(require('scuttlebot/plugins/friends'))
+  .use(require('./lib/friends-with-gossip-priority'))
   .use(require('scuttlebot/plugins/replicate'))
   .use(require('ssb-blobs'))
   .use(require('scuttlebot/plugins/invite'))
