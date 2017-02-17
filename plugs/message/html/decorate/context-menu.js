d2d97715e7cfb60e2d16cc9310db7fac8f3e6af7
Matt McKegg <matt@wetsand.co.nz>
2017-02-17 22:19:12 +1300

    add raw msg to Message elements for debugging

--- plugs/message/html/decorate/context-menu.js
+++ plugs/message/html/decorate/context-menu.js
@@ -6,6 +6,7 @@ exports.gives = nest('message.html.decorate')
 exports.create = (api) => {
   return nest('message.html.decorate', function (element, { msg }) {
     element.addEventListener('contextmenu', contextMenu.bind(null, msg))
+    element.msg = msg
     return element
   })
 }
