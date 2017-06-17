09d18ee84670767e89b5afcc9d5de810a6174aa5
Matt McKegg <matt@wetsand.co.nz>
2017-06-18 02:55:24 +1200

    whoops, remove patchwork-gatherings dep

--- main-window.js
+++ main-window.js
@@ -20,7 +20,6 @@ module.exports = function (config) {
     addCommand('app.navigate', setView),
     require('./modules'),
     require('./plugs'),
-    require('patchwork-gatherings'),
     require('patchcore'),
     require('./overrides')
   )
