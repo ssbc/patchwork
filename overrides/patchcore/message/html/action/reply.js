efead363d153b58ea67532a7044afee4c76bb99a
Matt McKegg <matt@wetsand.co.nz>
2018-02-20 20:04:34 +1300

    "reply" on last message replies to root message
    fixes #743

--- overrides/patchcore/message/html/action/reply.js
+++ overrides/patchcore/message/html/action/reply.js
@@ -1,13 +1,38 @@
 var h = require('mutant/h')
 var nest = require('depnest')
 
-exports.needs = nest({'intl.sync.i18n': 'first'})
+exports.needs = nest({
+  'intl.sync.i18n': 'first',
+  'app.navigate': 'first',
+  'message.sync.root': 'first'
+
+})
 
 exports.gives = nest('message.html.action')
 
 exports.create = (api) => {
   const i18n = api.intl.sync.i18n
   return nest('message.html.action', function reply (msg) {
-    return h('a', { href: msg.key, anchor: 'reply' }, i18n('Reply'))
+    return h('a', { href: msg.key, anchor: 'reply', 'ev-click': {handleEvent, api, msg} }, i18n('Reply'))
   })
+}
+
+function handleEvent (ev) {
+  var {api, msg} = this
+  var el = getMessageElement(ev.target)
+  
+  // HACK: if this is the last message in the list, reply to the root message
+  if (el && !el.nextElementSibling) {
+    api.app.navigate(api.message.sync.root(msg), 'reply')
+    ev.preventDefault()
+  }
+}
+
+function getMessageElement (el) {
+  while (el && el.classList) {
+    if (el.classList.contains('Message') && el.parentNode && el.parentNode.classList.contains('replies')) {
+      return el
+    }
+    el = el.parentNode
+  }
 }
