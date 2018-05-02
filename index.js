931f9ae90cf4da8951bc4688eb35db9c60c22ac7
Christian Bundy <christianbundy@fraction.io>
2018-05-02 15:48:01 -0700

    Upgrade scuttlebot and friends

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ electron.app.on('ready', () => {
 
   electron.ipcMain.on('open-background-devtools', function (ev, config) {
     if (windows.background) {
-      windows.background.webContents.openDevTools({detach: true})
+      windows.background.webContents.openDevTools({mode: 'detach'})
     }
   })
 })
