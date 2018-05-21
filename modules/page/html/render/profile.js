0f27cd3889bcb98a931409fcfd68a5ef24c163c1
neurodynamic <>
2018-05-21 08:10:06 -0700

    adds note about nicknames being public

--- modules/page/html/render/profile.js
+++ modules/page/html/render/profile.js
@@ -352,6 +352,11 @@ exports.create = function (api) {
               'font-weight': 'normal'
             }
           }, [i18n('What would you like to call '), h('strong', [currentName]), '?']),
+          h('p', {
+            style: {
+              'font-weight': 'normal'
+            }
+          }, [i18n('Nicknames you assign here will be publicly visible to others.')]),
           input
         ]),
         footer: [
