d2e75b85d48c0c4de980b3cde204994fa764ca45
Christian Bundy <christianbundy@fraction.io>
2018-08-28 07:39:45 -0700

    Produce diff-dark theme that sort of works

--- styles/compare.js
+++ styles/compare.js
@@ -1,43 +1,33 @@
-var tok = require('../node_modules/micro-css/lib/tokenizer.js')
-const fs = require('fs')
-// var diff = require('deep-diff')
-const _ = require('lodash')
+const fs = require('fs');
+const { diff } = require('deep-object-diff');
 
-const ttm = require('./token-to-mcss')
-
-const { diff, addedDiff, deletedDiff, detailedDiff, updatedDiff } = require("deep-object-diff");
-
-const dir = (...args) => console.dir(args, { depth: null }) 
-
-const commonRes = {}
-const darkRes = {}
-const lightRes = {}
+const tok = require('../node_modules/micro-css/lib/tokenizer.js');
+const ttm = require('./token-to-mcss');
 
 fs.readdir('./light', (err, files) => {
   Object.values(files).forEach((file) => {
     if (file.indexOf('.mcss') === -1) {
-      return false
-    } else {
-      fs.readFile(`./light/${file}`, 'utf-8', (lightErr, lightFile) => {
-        if (lightErr) throw lightErr;
-        fs.readFile(`./dark/${file}`, 'utf-8', (darkErr, darkFile) => {
-          if (darkErr) throw darkErr;
+      return false;
+    }
+    fs.readFile(`./light/${file}`, 'utf-8', (lightErr, lightFile) => {
+      if (lightErr) throw lightErr;
+      fs.readFile(`./dark/${file}`, 'utf-8', (darkErr, darkFile) => {
+        if (darkErr) throw darkErr;
 
-          lightTokens = tok(lightFile)
-          darkTokens = tok(darkFile)
+        const lightTokens = tok(lightFile);
+        const darkTokens = tok(darkFile);
 
-          // dir('light', lightTokens)
-          // dir('dark', darkTokens)
-          var style = diff(lightTokens, darkTokens)
-          var mcss = ttm(style)
-          if (mcss.length < 2) return
-          const result = mcss
+        const style = diff(lightTokens, darkTokens);
+        const mcss = ttm(style);
+        if (mcss.length < 2) return;
+        const result = mcss;
 
-          fs.writeFile(`./diff-dark/${file}`, result, (err) => {
-            if (err) throw err;
-          });
+        fs.writeFile(`./diff-dark/${file}`, result, (writeErr) => {
+          if (writeErr) throw writeErr;
         });
       });
-    }
-  })
-})
+    });
+
+    return true;
+  });
+});
--- styles/token-to-mcss.js
+++ styles/token-to-mcss.js
@@ -1,38 +1,31 @@
 module.exports = (tokens) => {
-  let result = ''
-  let indentation = 1;
-
-  const indent = () => indentation += 2 
-  const deindent = () => indentation -= 2
+  let result = '';
 
   const print = (text) => {
-    const indented = Array(indentation).join(' ') + text
-    result += indented
-    return indented
-  }
+    result += text;
+  };
 
   const walk = (obj, cb) => {
     Object.keys(obj).forEach((key) => {
-    if (obj[key]) {
-      if (key === 'objects' || key === 'flags' || key === 'pseudos' || key === 'entities' || key === 'elements' || key === 'mixins') {
-        cb(obj[key], cb)
-      } else if (key === 'rules') {
-        Object.keys(obj[key]).forEach((rule) => {
-          let value = obj[key][rule];
-          if (value !== undefined) {
-            print(rule + ': ' + value + ';\n')
-          }
-        });
-      } else {
-        print(key + ' {\n')
-        cb(obj[key], cb)
-        print('}\n')
+      if (obj[key]) {
+        if (key === 'objects' || key === 'flags' || key === 'pseudos' || key === 'entities' || key === 'elements' || key === 'mixins') {
+          cb(obj[key], cb);
+        } else if (key === 'rules') {
+          Object.keys(obj[key]).forEach((rule) => {
+            const value = obj[key][rule];
+            if (value !== undefined) {
+              print(`${rule}: ${value};\n`);
+            }
+          });
+        } else {
+          print(`${key} {\n`);
+          cb(obj[key], cb);
+          print('}\n');
+        }
       }
-    }
-    })
-  }
+    });
+  };
 
-  walk(tokens, walk)
-  console.log(result)
+  walk(tokens, walk);
   return result;
-}
+};
