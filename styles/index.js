7a029c08e9f5fb72ee3b92d27f52dacfd7ac9519
Christian Bundy <christianbundy@fraction.io>
2018-08-29 08:13:46 -0700

    Extract shared styles from light and dark themes
    This takes all of the shared MCSS styles from the light and dark themes
    and places them in a directory called `base`. This replaces the original
    theme directories with *only* the diffs from the shared MCSS styles, in
    an effort to reduce duplication and keep things DRY.

--- styles/index.js
+++ styles/index.js
@@ -1,4 +1,6 @@
+const base = require('./base')
+
 module.exports = {
-  light: require('./light'),
-  dark: require('./dark')
+  light: base + require('./light'),
+  dark: base + require('./dark')
 }
