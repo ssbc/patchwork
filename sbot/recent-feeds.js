47a73b1832cd6327035ea2f82273481ae6e2701a
Matt McKegg <matt@wetsand.co.nz>
2018-10-05 10:26:26 +1300

    fix stuck profiles in "Who to follow"

--- sbot/recent-feeds.js
+++ sbot/recent-feeds.js
@@ -1,5 +1,6 @@
 var pull = require('pull-stream')
 var pullCat = require('pull-cat')
+var getTimestamp = require('../lib/get-timestamp')
 
 module.exports = function (sbot, config) {
   return {
@@ -7,6 +8,10 @@ module.exports = function (sbot, config) {
       return pullCat([
         pull(
           sbot.createFeedStream({reverse: true, gt: since}),
+          pull.filter(msg => {
+            // filter out stuck items (also will be fixed by https://github.com/ssbc/secure-scuttlebutt/pull/215
+            return getTimestamp(msg) > since
+          }),
           pull.map(msg => msg.value.author),
           pull.unique()
         ),
