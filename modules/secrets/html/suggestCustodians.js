cb95ef45c4cc3322b6eed15a6b29a52dbaef2b70
Kieran Gibb <kieran@tenthousandthings.org.uk>
2019-03-21 12:51:03 +0000

    basic styling of form and insert share action

--- modules/secrets/html/suggestCustodians.js
+++ modules/secrets/html/suggestCustodians.js
@@ -1,6 +1,6 @@
 const nest = require('depnest')
 const { isFeedId } = require('ssb-ref')
-const { h, resolve, Struct } = require('mutant')
+const { h, resolve, Struct, Array: MutantArray } = require('mutant')
 const addSuggest = require('suggest-box')
 
 exports.gives = nest('secrets.html.suggestCustodians')
@@ -12,34 +12,41 @@ exports.needs = nest({
 exports.create = (api) => {
   const getProfileSuggestions = api.profile.async.suggest()
 
-  return nest('secrets.html.suggestCustodians', function (props) {
-    const state = Struct({
-      isEmpty: true
-    })
+  return nest('secrets.html.suggestCustodians', function (props, cb) {
+    const {
+      recps = MutantArray([])
+    } = props
 
-    let wasEmpty
+    const state = Struct({
+      isEmpty: true,
+      wasEmpty: null
+    })
 
     const suggestBox = h('input', {
       type: 'search',
       'ev-suggestselect': (ev) => {
         const { id: link, title: name } = ev.detail
-        const isPresent = resolve(props.recps).find(recp => recp === link || recp.link === link)
-        if (isPresent) return // can only add each recp once
-        if (props.recps.getLength() >= 7) return // max recps hard coded at 7
-        props.recps.push({ link, name })
+        const isPresent = resolve(recps).find(recp => recp === link || recp.link === link)
+
+        if (!isPresent) {
+          if (recps.getLength() >= 7) return // max recps hard coded at 7
+
+          recps.push({ link, name })
+        }
+
         ev.target.value = ''
         ev.target.placeholder = ''
       },
       'ev-keydown': (ev) => {
-        if (props.recps.getLength() >= 7 && !isBackspace(ev)) {
+        if (recps.getLength() >= 7 && !isBackspace(ev)) {
           ev.preventDefault()
           return false
         }
       },
       'ev-keyup': (ev) => {
-        state.isEmpty = wasEmpty && ev.target.value.length === 0
-        if (isBackspace(ev) && state.isEmpty && props.recps.getLength() > 2) props.recps.pop()
-        wasEmpty = ev.target.value.length === 0
+        state.isEmpty.set(resolve(state.wasEmpty) && ev.target.value.length === 0)
+        if (isBackspace(ev) && resolve(state.isEmpty) && recps.getLength() > 0) recps.pop()
+        state.wasEmpty.set(ev.target.value.length === 0)
       }
     })
 
