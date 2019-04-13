8d9ad984a3edac557927aeed0e9b54ce06e849bc
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-04-13 14:57:45 +0800

    Point people to discussing the blocked in a fork

--- modules/message/html/render/post.js
+++ modules/message/html/render/post.js
@@ -1,8 +1,13 @@
 var h = require('mutant/h')
+var mutantMap = require('mutant/map')
 var nest = require('depnest')
 var extend = require('xtend')
+var getRoot = require('../../../../lib/get-root')
 
 exports.needs = nest({
+  'keys.sync.id': 'first',
+  'profile.html.person': 'first',
+  'message.obs.get': 'first',
   'message.html': {
     decorate: 'reduce',
     layout: 'first',
@@ -23,9 +28,10 @@ exports.create = function (api) {
     canRender: isRenderable,
     render: function (msg, opts) {
       if (!isRenderable(msg)) return
+
       var element = api.message.html.layout(msg, extend({
         title: messageTitle(msg),
-        content: msg.isBlocked ? 'Content of a blocked user' : messageContent(msg),
+        content: msg.isBlocked ? blockedMessage(msg) : messageContent(msg),
         layout: 'default'
       }, opts))
 
@@ -37,6 +43,22 @@ exports.create = function (api) {
 
   function isRenderable (msg) {
     return (msg.value.content.type === 'post') ? true : undefined
+  }
+
+  function blockedMessage (msg) {
+    if (msg.blockedBy.role === 'me') {
+      return 'Content of a blocked user'
+    } else {
+      return h('div', {},
+        ['This post by ',
+          api.profile.html.person(msg.value.author),
+          ' is hidden because they are blocked by the thread author ',
+          api.profile.html.person(msg.blockedBy.id),
+          '. ',
+          h('a',{href: msg.key}, 'Click here'),
+          ' to view the post in a fork of this thread.'
+        ])
+    }
   }
 
   function messageContent (data) {
