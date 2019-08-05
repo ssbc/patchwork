103839fd4becb98c6161c0964ef73f96c2d66431
Christian Bundy <christianbundy@fraction.io>
2019-08-05 08:07:35 -0700

    Refactor to use let and const

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ process.on('uncaughtException', function (err) {
   process.exit()
 })
 
-var electron = require('electron')
-var openWindow = require('./lib/window')
+const electron = require('electron')
+const openWindow = require('./lib/window')
 
-var Path = require('path')
-var defaultMenu = require('electron-default-menu')
-var WindowState = require('electron-window-state')
-var Menu = electron.Menu
-var extend = require('xtend')
-var ssbKeys = require('ssb-keys')
+const Path = require('path')
+const defaultMenu = require('electron-default-menu')
+const WindowState = require('electron-window-state')
+const Menu = electron.Menu
+const extend = require('xtend')
+const ssbKeys = require('ssb-keys')
 
-var windows = {
+const windows = {
   dialogs: new Set()
 }
-var ssbConfig = null
-var quitting = false
+let ssbConfig = null
+let quitting = false
 
 /**
  * It's not possible to run two instances of patchwork as it would create two
@@ -40,7 +40,7 @@ function quitIfAlreadyRunning () {
   })
 }
 
-var config = {
+const config = {
   server: !(process.argv.includes('-g') || process.argv.includes('--use-global-ssb'))
 }
 // a flag so we don't start git-ssb-web if a custom path is passed in
@@ -54,8 +54,8 @@ electron.app.on('ready', () => {
   setupContext(process.env.ssb_appname || 'ssb', {
     server: !(process.argv.includes('-g') || process.argv.includes('--use-global-ssb'))
   }, () => {
-    var browserWindow = openMainWindow()
-    var menu = defaultMenu(electron.app, electron.shell)
+    const browserWindow = openMainWindow()
+    const menu = defaultMenu(electron.app, electron.shell)
 
     menu.splice(4, 0, {
       label: 'Navigation',
@@ -84,7 +84,7 @@ electron.app.on('ready', () => {
       ]
     })
 
-    var view = menu.find(x => x.label === 'View')
+    const view = menu.find(x => x.label === 'View')
     view.submenu = [
       { role: 'reload' },
       { role: 'toggledevtools' },
@@ -95,7 +95,7 @@ electron.app.on('ready', () => {
       { type: 'separator' },
       { role: 'togglefullscreen' }
     ]
-    var help = menu.find(x => x.label === 'Help')
+    const help = menu.find(x => x.label === 'Help')
     help.submenu = [
       {
         label: 'Learn More',
@@ -103,7 +103,7 @@ electron.app.on('ready', () => {
       }
     ]
     if (process.platform === 'darwin') {
-      var win = menu.find(x => x.label === 'Window')
+      const win = menu.find(x => x.label === 'Window')
       win.submenu = [
         { role: 'minimize' },
         { role: 'zoom' },
@@ -135,7 +135,7 @@ electron.app.on('ready', () => {
 
 function openMainWindow () {
   if (!windows.main) {
-    var windowState = WindowState({
+    const windowState = WindowState({
       defaultWidth: 1024,
       defaultHeight: 768
     })
